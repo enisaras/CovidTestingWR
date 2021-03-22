d7d6f7947a75df6297a7720ab6e46f9047c64245
arass <arass@rpi.edu>
2021-03-22 17:51:40 -0400

    Discord message

--- src/Components/run-form/form.js
+++ src/Components/run-form/form.js
@@ -1,13 +1,17 @@
+//General imports
 import React, { useState } from "react";
 import "./styles.css";
 import {Link} from 'react-router-dom';
+const webhook = require("webhook-discord");
+
 export default function Form() {
   const [formData, setFormData] = useState({});
   const [message, setMessage] = useState("");
   var formSubmitted = false;
+  const url = "https://v1.nocodeapi.com/enisaras/google_sheets/hTHdIarhyJkWmZVA?tabId=Sheet1";
 
   const handleInput = e => {
-      const copyFormData = { ...formData };
+    const copyFormData = { ...formData };
     copyFormData[e.target.name] = e.target.value;
     setFormData(copyFormData);
   };
@@ -17,33 +21,38 @@ export default function Form() {
     //TODO: Find better way to do this through POST
     if(!formSubmitted){
     try {
-      
+    
       const response = await fetch(
-        "https://v1.nocodeapi.com/enisaras/google_sheets/hTHdIarhyJkWmZVA?tabId=Sheet1",
+        url,
         {
           method: "post",
           body: JSON.stringify([[formData.name, formData.time]]),
           headers: {
             "Content-Type": "application/json"
           }
+         
         }
       );
       const json = await response.json();
-      console.log(json)
+      console.log(json);
       setMessage("Your run was succesfully submitted, it will be added to the leaderboard in less than 24 hours.");
     } catch (error) {
-      console.error("Error:", error);
+      console.error("Error: ", error);
       setMessage("Difficulty sending your run, please try again.");
       }
-      
+
     }
     else{
       setMessage("You already submitted a run today, you can submit another later.")
     }
-    formSubmitted = true;
+    const Hook = new webhook.Webhook(process.env.REACT_APP_WEBHOOK);
+    const msg = new webhook.MessageBuilder()
+                .setName("COVID Testing any% Leaderboard ")
+                .setText(`Name: ${formData.name} Time: ${formData.time}`);
+    Hook.send(msg);
     
   }
-  
+
     
 
   return (
